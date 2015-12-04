b7c28f6f396d0405db810d95eddde78029a157df
Chintan <chintan@wohlig.com>
2015-12-04 16:11:37 +0530

    Beautify code

--- js/app.js
+++ js/app.js
@@ -1,78 +1,75 @@
 // JavaScript Document
 var firstapp = angular.module('firstapp', [
-    'ui.router',
-    'phonecatControllers',
-    'templateservicemod',
-    'navigationservice'
+  'ui.router',
+  'phonecatControllers',
+  'templateservicemod',
+  'navigationservice'
 ]);
 
-firstapp.config(function($stateProvider, $urlRouterProvider,$httpProvider) {
+firstapp.config(function($stateProvider, $urlRouterProvider, $httpProvider) {
 
-    // for http request with session
-    $httpProvider.defaults.withCredentials = true;
+  // for http request with session
+  $httpProvider.defaults.withCredentials = true;
 
-    $stateProvider
+  $stateProvider
 
     .state('home', {
-        url: "/home",
-        templateUrl: "views/template.html",
-        controller: 'HomeCtrl'
-    })
+    url: "/home",
+    templateUrl: "views/template.html",
+    controller: 'HomeCtrl'
+  })
 
-    .state('slider', {
-        url: "/slider",
-        templateUrl: "views/template.html",
-        controller: 'SliderCtrl'
-    })
+  .state('slider', {
+    url: "/slider",
+    templateUrl: "views/template.html",
+    controller: 'SliderCtrl'
+  })
 
-    .state('love', {
-        url: "/love",
-        templateUrl: "views/template.html",
-        controller: 'LoveCtrl'
-    })
+  .state('love', {
+    url: "/love",
+    templateUrl: "views/template.html",
+    controller: 'LoveCtrl'
+  })
 
-    .state('lovedetail', {
-        url: "/lovedetail",
-        templateUrl: "views/template.html",
-        controller: 'LoveDetailCtrl'
-    })
+  .state('lovedetail', {
+    url: "/lovedetail",
+    templateUrl: "views/template.html",
+    controller: 'LoveDetailCtrl'
+  })
 
-    .state('lie', {
-        url: "/lie",
-        templateUrl: "views/template.html",
-        controller: 'LieCtrl'
-    })
+  .state('lie', {
+    url: "/lie",
+    templateUrl: "views/template.html",
+    controller: 'LieCtrl'
+  })
 
-    .state('lust', {
-        url: "/lust",
-        templateUrl: "views/template.html",
-        controller: 'LustCtrl'
-    })
+  .state('lust', {
+    url: "/lust",
+    templateUrl: "views/template.html",
+    controller: 'LustCtrl'
+  })
 
-    $urlRouterProvider.otherwise("/home");
+  $urlRouterProvider.otherwise("/home");
 
 });
 
 
 firstapp.directive('img', function($compile, $parse) {
-    return {
-        restrict: 'E',
-        replace: false,
-        link: function($scope, element, attrs) {
-            var $element = $(element);
-            if(!attrs.noloading)
-            {
-                $element.after("<img src='img/loading.gif' class='loading' />");
-                var $loading = $element.next(".loading");
-                $element.load(function() {
-                    $loading.remove();
-                    $(this).addClass("doneLoading");
-                });
-            }
-            else
-            {
-                $($element).addClass("doneLoading");
-            }
-        }
-    };
+  return {
+    restrict: 'E',
+    replace: false,
+    link: function($scope, element, attrs) {
+      var $element = $(element);
+      if (!attrs.noloading) {
+        $element.after("<img src='img/loading.gif' class='loading' />");
+        var $loading = $element.next(".loading");
+        $element.load(function() {
+          $loading.remove();
+          $(this).addClass("doneLoading");
+        });
+      } else {
+        $($element).addClass("doneLoading");
+      }
+    }
+  };
 });
