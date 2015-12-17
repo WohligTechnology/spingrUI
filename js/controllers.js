48956014a3a6c743bf39a9c65913ca6bfec6e24a
Chintan <chintan@wohlig.com>
2015-12-17 16:24:00 +0530

    LLL

--- js/controllers.js
+++ js/controllers.js
@@ -9,20 +9,22 @@ angular.module('phonecatControllers', ['templateservicemod', 'navigationservice'
     $scope.footercolor = "";
 
     $scope.mySlides = [
-    'http://flexslider.woothemes.com/images/kitchen_adventurer_cheesecake_brownie.jpg',
-    'http://flexslider.woothemes.com/images/kitchen_adventurer_lemon.jpg',
-    'http://flexslider.woothemes.com/images/kitchen_adventurer_donut.jpg',
-    'http://flexslider.woothemes.com/images/kitchen_adventurer_caramel.jpg'
-  ];
+      'http://flexslider.woothemes.com/images/kitchen_adventurer_cheesecake_brownie.jpg',
+      'http://flexslider.woothemes.com/images/kitchen_adventurer_lemon.jpg',
+      'http://flexslider.woothemes.com/images/kitchen_adventurer_donut.jpg',
+      'http://flexslider.woothemes.com/images/kitchen_adventurer_caramel.jpg'
+    ];
 })
 
-.controller('LoveCtrl', function ($scope, TemplateService, NavigationService) {
+.controller('LoveCtrl', function ($scope, TemplateService, NavigationService,$location) {
     //Used to name the .html file
     $scope.template = TemplateService.changecontent("love");
     $scope.menutitle = NavigationService.makeactive("Love");
     TemplateService.title = $scope.menutitle;
     $scope.navigation = NavigationService.getnav();
     $scope.footercolor = "black";
+
+    console.log();
 
 
 
@@ -40,31 +42,27 @@ angular.module('phonecatControllers', ['templateservicemod', 'navigationservice'
         $scope.viewed = data.mostViewed;
 
     };
-    NavigationService.getCategoryArticles(1, getArticles);
+
+
+    switch($location.path())
+    {
+      case "/love":
+      NavigationService.getCategoryArticles(1, getArticles);
+      console.log("Love");
+      break;
+      case "/lie":
+      NavigationService.getCategoryArticles(2, getArticles);
+      console.log("Lie");
+      break;
+      case "/lust":
+      NavigationService.getCategoryArticles(3, getArticles);
+      console.log("Lust");
+      break;
+
+
+    }
+
 })
-
-.controller('LieDetailCtrl', function ($scope, TemplateService, NavigationService) {
-        //Used to name the .html file
-        $scope.template = TemplateService.changecontent("liedetail");
-        $scope.menutitle = NavigationService.makeactive("Lie Detail");
-        TemplateService.title = $scope.menutitle;
-        $scope.navigation = NavigationService.getnav();
-        $scope.footercolor = "black";
-    })
-    .controller('ConfessionDetailCtrl', function ($scope, TemplateService, NavigationService) {
-        //Used to name the .html file
-        $scope.template = TemplateService.changecontent("confessiondetail");
-        $scope.menutitle = NavigationService.makeactive("Confession Detail");
-        TemplateService.title = $scope.menutitle;
-        $scope.navigation = NavigationService.getnav();
-        $scope.footercolor = "black";
-
-        $scope.confessionblog = [{
-            head: "#51",
-            title: "I have heard a lot about women pleasuring themselves and reaching orgasm without a man. I want to try it out.",
-            content: "I was into a relationship for 5 years.. I loved him more than anything in this world. But we always fought. I never was loved by anyone.. Not by parents, not by sisters, friends betrayed me. Even he started behaving which i never expectd. Made fun of me always, stopped hearing my problem, he just stopped talking with me. We lived together but he was never there with me. Ignored me for his friends, family.. Then he would always spend my money and will say he has spent alot on me which i dont even know. I was into a relationship for 5 years.. I loved him more than anything in this world. But we always fought. I never was loved by anyone.. Not by parents, not by sisters, friends..."
-    }];
-    })
 
 .controller('ConfessionsCtrl', function ($scope, TemplateService, NavigationService) {
     //Used to name the .html file
@@ -84,115 +82,6 @@ angular.module('phonecatControllers', ['templateservicemod', 'navigationservice'
         content: "I have heard a lot about women pleasuring themselves and reaching orgasm without a man. I want to try it out. Are there any precautions that I need to take?"
   }];
 })
-
-.controller('LieCtrl', function ($scope, TemplateService, NavigationService) {
-    //Used to name the .html file
-    $scope.template = TemplateService.changecontent("lie");
-    $scope.menutitle = NavigationService.makeactive("Lie");
-    TemplateService.title = $scope.menutitle;
-    $scope.navigation = NavigationService.getnav();
-    console.log("my ctrol");
-    $scope.demo = "demo working";
-    $scope.footercolor = "black";
-
-    $scope.liesblog = [{
-            img: "img/love-pic.jpg",
-            title: "Love for Two, Please!",
-            date: "March 13, 2015 by Anonymous",
-            descp: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipisicing elit. "
-    }, {
-            img: "img/kiss-pic.jpg",
-            title: "Love for Two, Please!",
-            date: "March 13, 2015 by Anonymous",
-            descp: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipisicing elit. "
-    }, {
-            img: "img/love-pic.jpg",
-            title: "Love for Two, Please!",
-            date: "March 13, 2015 by Anonymous",
-            descp: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipisicing elit. "
-    },
-
-        {
-            img: "img/kiss-pic.jpg",
-            title: "Love for Two, Please!",
-            date: "March 13, 2015 by Anonymous",
-            descp: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipisicing elit. "
-    }, {
-            img: "img/love-pic.jpg",
-            title: "Love for Two, Please!",
-            date: "March 13, 2015 by Anonymous",
-            descp: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipisicing elit. "
-    },
-
-        {
-            img: "img/kiss-pic.jpg",
-            title: "Love for Two, Please!",
-            date: "March 13, 2015 by Anonymous",
-            descp: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipisicing elit. "
-    }
-  ];
-    console.log($scope.liesblog);
-    $scope.viewed = [{
-        img: "img/kiss-pic.jpg",
-        caption: "Butterfly Junkie"
-  }, {
-        img: "img/post2.jpg",
-        caption: "Butterfly Junkie"
-  }, {
-        img: "img/kiss-pic.jpg",
-        caption: "Butterfly Junkie"
-  }, {
-        img: "img/post2.jpg",
-        caption: "Butterfly Junkie"
-  }];
-})
-
-.controller('LustCtrl', function ($scope, TemplateService, NavigationService) {
-        //Used to name the .html file
-        $scope.template = TemplateService.changecontent("lust");
-        $scope.menutitle = NavigationService.makeactive("Lust");
-        TemplateService.title = $scope.menutitle;
-        $scope.navigation = NavigationService.getnav();
-    })
-    .controller('SexpertCtrl', function ($scope, TemplateService, NavigationService) {
-        //Used to name the .html file
-        $scope.template = TemplateService.changecontent("sexpert");
-        $scope.menutitle = NavigationService.makeactive("Sexpert");
-        TemplateService.title = $scope.menutitle;
-        $scope.navigation = NavigationService.getnav();
-
-        $scope.footercolor = "black";
-
-        $scope.sexpertblog = [{
-                content: "I have just graduated and have been offered my dream job in Mumbai. But it will be moving away from my family and my boyfriend. I don’t know if i will be able to live without him."
-    }, {
-                content: "I am 18 and i met this boy on my holiday to Himachal Pradesh. Now I am back home and i miss him terribly. He is a local and we’ve kept in touch. I want to go back there for my Christmas vacation, but my parents wont allow it. I feel like running away to him. How do i convince my parents to let me go?"
-    }, {
-                content: "I have just graduated and have been offered my dream job in Mumbai. But it will be moving away from my family and my boyfriend. I don’t know if i will be able to live without him."
-    },
-            {
-                content: "I am 18 and i met this boy on my holiday to Himachal Pradesh. Now I am back home and i miss him terribly. He is a local and we’ve kept in touch. I want to go back there for my Christmas vacation, but my parents wont allow it. I feel like running away to him. How do i convince my parents to let me go?"
-    }];
-    })
-    .controller('QueriesCtrl', function ($scope, TemplateService, NavigationService) {
-        //Used to name the .html file
-        $scope.template = TemplateService.changecontent("queries");
-        $scope.menutitle = NavigationService.makeactive("Queries");
-        TemplateService.title = $scope.menutitle;
-        $scope.navigation = NavigationService.getnav();
-        $scope.footercolor = "black";
-        $scope.queriesblog = [{
-                content: "I have just graduated and have been offered my dream job in Mumbai. But it will be moving away from my family and my boyfriend. I don’t know if i will be able to live without him."
-    }, {
-                content: "I am 18 and i met this boy on my holiday to Himachal Pradesh. Now I am back home and i miss him terribly. He is a local and we’ve kept in touch. I want to go back there for my Christmas vacation, but my parents wont allow it. I feel like running away to him. How do i convince my parents to let me go?"
-    }, {
-                content: "I have just graduated and have been offered my dream job in Mumbai. But it will be moving away from my family and my boyfriend. I don’t know if i will be able to live without him."
-    },
-            {
-                content: "I am 18 and i met this boy on my holiday to Himachal Pradesh. Now I am back home and i miss him terribly. He is a local and we’ve kept in touch. I want to go back there for my Christmas vacation, but my parents wont allow it. I feel like running away to him. How do i convince my parents to let me go?"
-                          }
-                         ];
-    })
 
 .controller('AboutusCtrl', function ($scope, TemplateService, NavigationService) {
         //Used to name the .html file
@@ -252,14 +141,6 @@ angular.module('phonecatControllers', ['templateservicemod', 'navigationservice'
     TemplateService.title = $scope.menutitle;
     $scope.navigation = NavigationService.getnav();
     $scope.footercolor = "black";
-})
-
-.controller('LustCtrl', function ($scope, TemplateService, NavigationService) {
-    //Used to name the .html file
-    $scope.template = TemplateService.changecontent("lust");
-    $scope.menutitle = NavigationService.makeactive("Lust");
-    TemplateService.title = $scope.menutitle;
-    $scope.navigation = NavigationService.getnav();
 })
 
 .controller('footerCtrl', function ($scope, TemplateService, NavigationService) {
